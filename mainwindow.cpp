323bd635bcf388340b45a559afdf5f38171ffd36
notan79 <njcrutchfield@gmail.com>
2024-01-12 17:28:47 -0700

    Added comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,9 @@
 #include <random>
-#include <iostream>
 #include <cstdlib>
 #include <filesystem>
 #include <thread>
 //#include <fstream>
+//#include <iostream>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -23,12 +23,14 @@ MainWindow::MainWindow(QWidget *parent)
     player->setAudioOutput(audio_output);
 
 
+    // Setting picture for each of the GUI elements
     ui->button_play->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
     ui->button_shuffle->setIcon(style()->standardIcon(QStyle::SP_BrowserReload));
     ui->button_backward->setIcon(style()->standardIcon(QStyle::SP_MediaSkipBackward));
     ui->button_forward->setIcon(style()->standardIcon(QStyle::SP_MediaSkipForward));
 
 
+    // Checks for songs in the given path
     const std::string PATH = "/Users/nc/song_downloads";
     update_songs(PATH);
 
@@ -37,18 +39,25 @@ MainWindow::MainWindow(QWidget *parent)
         ui->label->setText("Download a song before playing");
         ui->horizontalSlider->setEnabled(false);
     }
-
-    else{
+    else
+    {
         player->setSource(QUrl(QString::fromStdString(PATH+"/")+songs[0]));
         update_text();
     }
+
+    // Initializes the slider to the audio
     connect(player, &QMediaPlayer::durationChanged, ui->horizontalSlider, &QSlider::setMaximum);
     connect(player, &QMediaPlayer::positionChanged, ui->horizontalSlider, &QSlider::setValue);
-    connect(ui->horizontalSlider, &QSlider::sliderMoved, this, [=](){
+
+    // Updates song position when the slider is moved
+    connect(ui->horizontalSlider, &QSlider::sliderMoved, this, [=]()
+    {
         player->setPosition(ui->horizontalSlider->sliderPosition());
+
+        // Case if slider reaches the end of the audio
         if(ui->horizontalSlider->sliderPosition() == ui->horizontalSlider->maximum())
         {
-            ui->horizontalSlider->setEnabled(false);
+            ui->horizontalSlider->setEnabled(false);    // Stops from skipping until slider released
             this->next_song();
             std::this_thread::sleep_for(std::chrono::milliseconds(200));
             ui->horizontalSlider->setValue(0);
@@ -57,6 +66,7 @@ MainWindow::MainWindow(QWidget *parent)
         }
     });
 
+    // Goes to next song when reaching the end of the audio
     connect(player, &QMediaPlayer::mediaStatusChanged, this, [=](){
         if(player->mediaStatus() == QMediaPlayer::EndOfMedia){
             this->next_song();
@@ -72,6 +82,14 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_button_play_pressed()
 {
+/*
+    Method to start the audio media when the button is pressed. Updates
+    the button to represent what the next action will do.
+
+    Parameters: None
+
+    Returns: None
+*/
     if(songs.length() == 0)
         return;
     if(player->isPlaying()){
@@ -85,9 +103,17 @@ void MainWindow::on_button_play_pressed()
 }
 
 
-// shuffle button
 void MainWindow::on_button_shuffle_pressed()
 {
+/*
+    Method to shuffle the audio files and update the list to the new
+    shuffled state. Keeps the current audio as the first. Will update
+    the 'songs' instance variable.
+
+    Parameters: None
+
+    Returns: None
+*/
     if(songs.length() == 0)
         return;
 
@@ -113,6 +139,14 @@ void MainWindow::on_button_shuffle_pressed()
 
 void MainWindow::next_song()
 {
+/*
+    Method to play the next song in the list. Removes the first song and adds
+    it to the end of the list.
+
+    Parameters: None
+
+    Returns: None
+*/
     QString cur = songs[0];
     songs.remove(0);
     songs.append(cur);
@@ -125,6 +159,13 @@ void MainWindow::next_song()
 
 void MainWindow::update_text()
 {
+/*
+    Method to update the text to show the current and next song.
+
+    Parameters: None
+
+    Returns: None
+*/
     QString text;
     if(songs.length() == 0)
         return;
@@ -138,6 +179,14 @@ void MainWindow::update_text()
 
 void MainWindow::on_button_backward_pressed()
 {
+/*
+    Method to go to the previous song in the list when the button is
+    pressed.
+
+    Parameters: None
+
+    Returns: None
+*/
     if(songs.length() == 0)
         return;
     bool wasPlaying = player->isPlaying();
@@ -155,6 +204,14 @@ void MainWindow::on_button_backward_pressed()
 
 void MainWindow::on_button_forward_pressed()
 {
+/*
+    Method to go to the next song in the list when the button is
+    pressed.
+
+    Parameters: None
+
+    Returns: None
+*/
     if(songs.length() == 0)
         return;
     bool wasPlaying = player->isPlaying();
@@ -163,44 +220,53 @@ void MainWindow::on_button_forward_pressed()
         player->play();
 }
 
-// bool MainWindow::youtube_download(std::string url){
-//     const std::string PATH = "/qrc:/sounds/";
-//     const std::string COMMAND = $ yt-dlp "https://www.youtube.com/watch?v=taWjtvyhWOw" -xciw -f "bestaudio/best" --audio-format mp3 --ffmpeg-location "audio-orchestrator-ffmpeg/bin" -P "song_downloads"
-//     return system(COMMAND.c_str());
-// }
-
-
 void MainWindow::on_button_download_pressed()
 {
+/*
+    Initiates the download process of a youtube audio when the button
+    is pressed.
+
+    Parameters: None
+
+    Returns: None
+*/
     const std::string YT_URL = ui->text_url->toPlainText().toStdString();
     const std::string PATH = "/Users/nc/song_downloads";
     const std::string COMMAND = "/Users/nc/anaconda3/bin/yt-dlp \"" + YT_URL + "\" -xciw -f \"bestaudio/best\" --audio-format mp3 --ffmpeg-location \"/Users/nc/audio-orchestrator-ffmpeg/bin\" -P \"" + PATH + "\"";
-    system(COMMAND.c_str());
+    system(COMMAND.c_str());    // Runs the yt-dlp command line program
     update_songs(PATH);
 
-    // this stuff was for updating a resource folder//
+
+
+    // this code was for updating a resource folder//
 
     //std::string qrc = get_qrc(PATH);
     // const std::string res = "/Users/nc/QT_Projects/MP3_Player/res.qrc";
     // std::ofstream file_write(res, std::ios::trunc);
     // file_write << qrc;
     // file_write.close();
-
 }
 
 void MainWindow::update_songs(const std::string PATH)
 {
+/*
+    Method to update the 'songs' instance variable when a new song
+    is downloaded.
 
+    Parameters: 'const std::string PATH': The path where the audio
+                 files are located.
+
+    Returns: None
+*/
 
     QList<QString> temp;
     std::string cur = "";
     if(songs.length() != 0)
         std::string cur = songs[0].toStdString();
 
-    for (const auto & entry : std::filesystem::directory_iterator(PATH)){
+    for (const auto &entry : std::filesystem::directory_iterator(PATH)){
         std::string str = entry.path();
         str.erase(0, PATH.length()+1); // removes the path for the string
-        std::cout << str;
         std::string extension = str;
         extension.erase(0,extension.length()-3); // gets the file type
 
@@ -210,7 +276,7 @@ void MainWindow::update_songs(const std::string PATH)
         }
     }
     if(cur != "")
-        temp.prepend(QString::fromStdString(cur));
+        temp.prepend(QString::fromStdString(cur)); // keeps the same current song playing
 
     songs = temp;
     if(songs.length() != 0)
@@ -251,9 +317,3 @@ std::string MainWindow::get_qrc(const std::string PATH)
     songs = temp;
     return text;
 }
-
-// // Prints out the strings of the names in the songs directory
-// for (const auto & entry : std::filesystem::directory_iterator(PATH)){
-//     str = entry.path();
-//     std::cout << str.erase(0,PATH.length()+1) << std::endl;
-// }
